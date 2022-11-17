61fd9dd2c3622aeb94c7992b170018e437b7d08c
XandyNinfadora <xandriellepessanhax@gmail.com>
2022-11-16 21:23:40 -0300

    projeto iniciado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.css'
+
+function Cabecalho() {
+
+
+  return (
+    <div className={styles.cabecalho2}>
+<h1>gANHAMO</h1>
+    </div>
+    
+  )
+}
+
+export default Cabecalho
