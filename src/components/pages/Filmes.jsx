4522fc04672de2828aae5988ca0b3ae4714abd9b
XandyNinfadora <xandriellepessanhax@gmail.com>
2022-11-23 19:20:48 -0300

    +1 blibioteca, animação entre as páginas e +1 pág

--- src/components/pages/Filmes.jsx
+++ src/components/pages/Filmes.jsx
@@ -1,6 +1,14 @@
-function Filmes () {
+import { motion } from 'framer-motion';
+import styles from './Filmes.css';
+
+function Filmes() {
     return (
-        <h1>Filmes GANHAMO</h1>
+        <motion.div className={styles.divPage}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }} >
+            <h1>Filmes GANHAMO</h1>
+        </motion.div>
     )
-    }
-    export default Filmes;
+}
+export default Filmes;
