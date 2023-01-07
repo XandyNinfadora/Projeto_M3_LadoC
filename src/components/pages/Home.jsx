bd24b159b4ab0791c11755d9fcb7240d38f99bed
XandyNinfadora <xandriellepessanhax@gmail.com>
2023-01-07 11:55:37 -0300

    Ajeitando foto home cinema

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,10 +10,10 @@ function Home() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }} >
             <div className={styles.homeDiv}>
-                <Link to="/filmes"><img id="fotosVai" src={fotoHome} /></Link>
+                <Link to="/filmes"><img id="fotoHome" src={fotoHome} /></Link>
                 <div className='homeDiv'>
                     <h1> Sobre Nós</h1>
-                    <p className='post' id='postDif'>  Somos uma empresa familiar que pensamos na proposta de trazer filmes antigos e clássicos de volta as telonas. Existe uma demanda grande do mercado por cinemas que repetem filmes recentes ou mesmo antigos, e é exatamente esse público que visamos. Trazendo filmes premiados e um catálogo diferente em cada mês, além dos nossos eventos mensais e semestrais. Venha participar dessa aventura espetacular em rever filmes incríveis e experiências nostálgicas!
+                    <p className='sobre' id='sobreHome'>  Somos uma empresa familiar que pensamos na proposta de trazer filmes antigos e clássicos de volta as telonas. Existe uma demanda grande do mercado por cinemas que repetem filmes recentes ou mesmo antigos, e é exatamente esse público que visamos. Trazendo filmes premiados e um catálogo diferente em cada mês, além dos nossos eventos mensais e semestrais. Venha participar dessa aventura espetacular em rever filmes incríveis e experiências nostálgicas!
                     </p>
                 </div>
             </div>
