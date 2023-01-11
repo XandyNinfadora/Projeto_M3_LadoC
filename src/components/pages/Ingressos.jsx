c7882aad7ded87ba14965ab39d7b3482959e5550
XandyNinfadora <xandriellepessanhax@gmail.com>
2023-01-11 16:04:34 -0300

    alterando css ingressos e classnames

--- src/components/pages/Ingressos.jsx
+++ src/components/pages/Ingressos.jsx
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion';
-import styles from './Ingressos.css';
+import styles from './Ingressos.module.css';
+
+/*refazer esse componente */
 
 function Ingressos() {
   return (
@@ -7,18 +9,18 @@ function Ingressos() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }} >
-      <div id="body2">
-        <div className="cardFilial">
+      <div className={styles.ingressoDiv}>
+        <div className="cardFilia">
           <div className="textoFilial2">
-            <h1 className="nomeFilial2">Sexta, Sábado, Domingo e Feriados</h1>
+            <h1 className={styles.cardIngressoTexto}>Sexta, Sábado, Domingo e Feriados</h1>
             <p className="enderecoFilial2">Já estão no valor de R$ 12,99 a meia-entrada, e R$ 22,99 a inteira.</p>
           </div>
 
 
         </div>
         <div className="cardFilial">
-          <div className="textoFilial2">
-            <h1 className="nomeFilial2">Segunda, Terça, Quarta e Quinta</h1>
+          <div className={styles.cardIngressoTexto}>
+            <h1 className="casa">Segunda, Terça, Quarta e Quinta</h1>
             <p className="enderecoFilial2"> Estão pelo preço promocional, R$ 9,99 a meia-entrada e a inteira por R$ 19,99!</p>
           </div>
         </div>
