58a13ae4cd6bb3ad9c082282dc687721828b7c5d
Andrielle Pessanha <xandriellepessanhax@gmail.com>
2023-03-21 16:17:15 -0300

    editando

--- src/components/configAxios.js
+++ src/components/configAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const blogFetch = axios.create({
-    baseURL: "https://ladocapi.onrender.com/",
+    baseURL: "http://localhost:3000/",
     headers: {
         "Content-Type": "application/json",
     },
